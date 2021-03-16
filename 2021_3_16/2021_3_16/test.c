27e10c94e85ba1cbad70f0a2cb50b87a5e5894ca
未然 <220673028@qq.com>
2021-03-16 21:45:27 +0800

    宏定义  学习中

---
+++ 2021_3_16/2021_3_16/test.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS  1
+
+#include<stdio.h>
+//声明add函数
+//extern int add(int, int);
+#define SQUARE(x) x*x;
+int main()
+{
+	//int a = 10;
+	//int b = 20;
+	////add函数执行a+b
+	//int ret = add(a, b);
+	//printf("%d", ret);
+
+	//int i = 0;
+	//for (i = 0; i < 10; i++)
+	//{
+	//	//function
+	//	//打印函数名
+
+	//	printf("%s\n", __FUNCTION__);
+	//	//FILE 打印了函数名 LINE打印了 该代码的行数 TIME打印了执行该代码的时间
+	//	/*printf("file:%s-line:%d -time:%s;%d\n",__FILE__ ,__LINE__,__TIME__,i);*/
+	//}
+
+	return 0;
+
+}
